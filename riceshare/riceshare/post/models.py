5d9a03f070e480963ca96a16dbd20614d331e157
Liping Sun <lipsun@indiana.edu>
2017-05-15 15:54:58 -0700

    implement feed feature

--- riceshare/riceshare/post/models.py
+++ riceshare/riceshare/post/models.py
@@ -5,3 +5,5 @@ class Post(models.Model):
     user = models.ForeignKey(User)
     post = models.CharField(max_length = 500)
     image = models.ImageField(null = True, blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- riceshare/riceshare/post/views.py
+++ riceshare/riceshare/post/views.py
@@ -5,6 +5,7 @@ from riceshare.post.models import Post
 
 
 def post_home(request):
+
     post_form = PostForm(request.POST or None, request.FILES or None)
     if request.method == "POST":
         if post_form.is_valid():
@@ -12,8 +13,13 @@ def post_home(request):
             post.save()
             return HttpResponse("Post Successuflly")
     else:
-        post_form = PostForm()
-        return render(request, "post/post_home.html", context={"post_form": post_form})
 
+        post_form = PostForm()
+
+        user = request.user
+        posts = set()
+        for post in user.post_set.all():
+            posts.add(post)
+        return render(request, "post/post_home.html", context={"post_form": post_form, "posts": posts})
 
 
